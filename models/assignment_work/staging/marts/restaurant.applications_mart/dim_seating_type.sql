9db33bfbf9ddd374f4ce451becaf0c320faf21e6
Nicole Malescio <nicolemalescio@gmail.com>
2026-03-27 17:06:30 +0000

    final dim_seating_type

--- models/assignment_work/staging/marts/restaurant.applications_mart/dim_seating_type.sql
+++ models/assignment_work/staging/marts/restaurant.applications_mart/dim_seating_type.sql
@@ -2,10 +2,16 @@
 WITH seating_types AS (
    SELECT DISTINCT
        seating_interest_sidewalk AS seating_interest,
---TODO: Replace this comment with a CASE WHEN .. statement that handles the different possibilities for approved_for_sidewalk_seating and approved_for_roadway_seating in the data
+        CASE 
+            WHEN approved_for_sidewalk_seating = "yes" THEN TRUE
+            ELSE FALSE
+        END AS approved_for_sidewalk,  
+        CASE 
+            WHEN  approved_for_roadway_seating = "yes" THEN TRUE
+            ELSE FALSE
+        END AS approved_for_roadway   --TODO: Replace this comment with a CASE WHEN .. statement that handles the different possibilities for approved_for_sidewalk_seating and approved_for_roadway_seating in the data
 --NOTE: The final result we want to select here is two boolean columns (TRUE or FALSE values in them), one column approved_for_sidewalk (TRUE or FALSE value), and one column approved_for_roadway 
-      AS approved_for_roadway
-   FROM --TODO: reference the appropriate staging table!
+   FROM {{ ref('stg_nyc_open_restaurant_apps') }} --TODO: reference the appropriate staging table!
    WHERE seating_interest_sidewalk IS NOT NULL
 ),
 seating_dimension AS (
@@ -15,10 +21,9 @@ seating_dimension AS (
            'approved_for_sidewalk',
            'approved_for_roadway'
        ]) }} AS seating_type_key,
-
-       -- TODO: fill in the rest of this SELECT statement
-       --  based on the dimensional model!
-
+        seating_interest, 
+        approved_for_sidewalk,
+        approved_for_roadway
    FROM seating_types
 )
 
