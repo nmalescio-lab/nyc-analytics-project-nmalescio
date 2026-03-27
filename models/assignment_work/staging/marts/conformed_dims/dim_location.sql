166acaac415f2418e038a3cb6c27030a42f71558
Nicole Malescio <nicolemalescio@gmail.com>
2026-03-27 05:36:46 +0000

    name update

--- models/assignment_work/staging/marts/conformed_dims/dim_location.sql
+++ models/assignment_work/staging/marts/conformed_dims/dim_location.sql
@@ -13,7 +13,7 @@ WITH all_locations AS (
 
    -- Get locations from restaurant applications
    SELECT DISTINCT
-       CAST(borough AS STRING) AS location_borough,
+       CAST(borough AS STRING) AS borough,
        CAST(zip AS STRING) AS zip_code -- TODO replace (HINT: look @ dimensional model & staging data!)
    FROM {{ ref('stg_nyc_open_restaurant_apps') }}
    WHERE borough IS NOT NULL
